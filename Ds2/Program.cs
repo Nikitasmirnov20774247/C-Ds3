cb0572b233f6ab8495bb0b89ea72b0976f2e122e
Nikita <Nikitasmirnov2000@yandex.ru>
2022-08-15 18:49:58 +0300

    ds2

---
+++ Ds2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите координату x для точки a: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y для точки a: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z для точки a: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату x для точки b: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y для точки b: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z для точки b: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double formula = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+double Distance = Math.Round (formula, 2 );
+
+Console.WriteLine($"Длина отрезка в 3D пространстве = {Distance}");
