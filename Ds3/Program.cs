01e88254ab0f63d3307633c01e52cdf0d39f956f
Nikita <Nikitasmirnov2000@yandex.ru>
2022-08-15 18:50:22 +0300

    ds3

---
+++ Ds3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите целое число N больше 1: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+if (Num > 1)
+{
+    Console.WriteLine($"Таблица кубов чисел от 1 до {Num}: ");
+    for (int i = 0; i < Num; i++)
+    {
+        Console.Write(Math.Pow(i+1, 3) + ", ");
+    }
+}
+else
+{
+    Console.WriteLine("!! Введёное число меньше или равно еденице !!");
+}
